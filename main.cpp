90406ae0bb349acdddac4deb3e4156eba09e186f
Vipin-1999 <vipinrbharadwaj@gmail.com>
2019-07-31 21:16:58 +0530

    Final Commit

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()      {
     Pincode p;
-    p.Ccode(); //to add codes from te.txt to vector
+    p.Ccode(); //to add codes from code.txt to vector
     // cout << p.Random(0, "560") << endl;
     cout << p.Validate("560078") << endl;
     return 0;
